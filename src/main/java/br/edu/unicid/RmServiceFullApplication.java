5dfd4142278934529394e37a4b08d34a4383d263
Ricardo Fernando <ricardo347@msn.com>
2020-01-31 11:46:48 -0300

    Initial Commit

---
+++ src/main/java/br/edu/unicid/RmServiceFullApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unicid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmServiceFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmServiceFullApplication.class, args);
+	}
+
+}
